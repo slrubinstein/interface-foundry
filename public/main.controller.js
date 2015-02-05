3955ce41c59d45e8d511b54cf314a54f9de4f37c
Steve Rubinstein <slrubinstein@gmail.com>
2015-02-05 12:49:51 -0500

    restructure bubbles into stacks of 3

--- public/main.controller.js
+++ public/main.controller.js
@@ -1,21 +1,37 @@
 angular.module('ifApp')
 	.controller('mainController', mainController);
 
-mainController.$inject = ['$scope', 'dataService'];
+mainController.$inject = ['dataService'];
 
-function mainController($scope, dataService) {
+function mainController(dataService) {
 
 	var vm = this;
 
 	vm.bubbles = [];
-	vm.test = 'angular'
+	vm.stacks = [];
+	vm.groupByThrees = groupByThrees;
+	vm.test = 'angular';
 
 	activate();
 
 	function activate() {
 		dataService.get().then(function(result) {
 			vm.bubbles = result.data;
+
+			while (vm.bubbles.length) {
+				var stack ={
+					topCard: vm.bubbles.shift(),
+					midCard: vm.bubbles.shift(),
+					botCard: vm.bubbles.shift()
+				}
+				vm.stacks.push(stack)
+			}
+			console.log(vm.stacks)
 		});
+	}
+
+	function groupByThrees(index) {
+		return index % 3 === 0;
 	}
 
 }
