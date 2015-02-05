f03f40492e649cf3b82370c100cbf3b677577611
Steve Rubinstein <slrubinstein@gmail.com>
2015-02-05 13:49:55 -0500

    add shapes to cards

--- public/data.service.js
+++ public/data.service.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('ifApp')
 	.factory('dataService', dataService);
 
---
+++ public/shapes.directive.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('ifApp')
+  .directive('shapes', function () {
+    // return {
+    //   restrict: 'A',
+    //   scope: '@',
+    //   link: function (scope, element, attrs) {
+    //     attrs.$observe('shapes', function(value) {
+    //       console.log(value)
+    //     })
+    //   }
+    // };
+  });
